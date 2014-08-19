a64ef4d33acada649f7f8eb67db29e0d530b6a0f
Hiroshi Sakurai <sakurai777@gmail.com>
2014-08-19 10:07:02 +0900

    added strollmincaml.rb

---
+++ strollmincaml.rb
@@ -0,0 +1,32 @@
+require "formula"
+
+class Strollmincaml < Formula
+  url "https://github.com/hsk/strollmincaml.git", :branch => 'master'
+  homepage "http://github.com/hsk/strollmincaml"
+  head "https://github.com/hsk/strollmincaml.git", :using => :git
+  sha1 ""
+  version 'latest'
+
+  depends_on 'ocaml'
+
+  bottle do
+    cellar :any
+    sha1 "96c6dbf24b68977e02807b68e5c42575958e857c" => :mavericks
+  end
+
+  bottle do
+    cellar :any
+    root_url "https://raw.githubusercontent.com/hsk/homebrew-tap/master/bottles"
+    sha1 "" => :mavericks
+  end
+
+  def install
+    #ENV.deparallelize
+
+    ENV["PATH"] = ENV["PATH"] + ':' + ENV["HOME"] +'/.opam/system/bin'
+    system "cd l16polyinfer; "+ENV["HOME"]+"/.opam/system/bin/omake"
+    system "mv l16polyinfer/lllc.opt ./lllc"
+    bin.install Dir["lllc"]
+  end
+
+end
